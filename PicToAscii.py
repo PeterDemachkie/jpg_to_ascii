a47a882960256ab5b9d7068d76fd90421b777495
Peter Demachkie <peterdemachkie@Peters-MacBook-Pro.local>
2023-02-20 00:51:36 -0800

    Adding PicToAscii.py and README.d

---
+++ PicToAscii.py
@@ -0,0 +1,48 @@
+from PIL import Image
+import sys, os
+
+img_file = str(sys.argv[1])
+
+if len(sys.argv) != 2:
+    raise ValueError("Usage: python ascii_to_jpg.py <filename>")
+if img_file [-4:] != ".jpg":
+    raise Exception("File Error: only .jpg filetypes allowed\n")
+if not os.path.isfile(img_file):
+    raise FileNotFoundError(f"{img_file} does not exist in the working directory")
+
+SaveName = img_file[:-4] + 'Ascii.txt'
+
+imgcolor = Image.open(img_file)
+img = imgcolor.convert('L')
+width, height = img.size
+
+if width > 100:
+    mult = (2 * round((width/100), 2))
+    width = 100
+    height = int(height/mult)
+    img = img.resize((width, height))
+
+format = []
+for y in range(height):
+    for x in range(width):
+        color = img.getpixel((x, y))
+        if color >= 205:
+            format.append('@')
+        if 205 > color >= 185:
+            format.append('#')
+        if 185 > color >= 150:
+            format.append('*')
+        if 150 > color >= 120:
+            format.append(';')
+        if 120 > color >= 90:
+            format.append('.')
+        if 90 > color:
+            format.append(' ')
+    format.append('\n')
+final = ''.join(format)
+
+File = open(SaveName, 'w')
+for letter in final:
+    File.write(letter)
+File.close()
+
---
+++ README.md
@@ -0,0 +1,23 @@
+Peter Demachkie
+jpg_to_ascii
+https://github.com/PeterDemachkie/jpg_to_ascii
+
+Repository Contents:
+
+    1) README.md: (This file) a brief description of the repository and its contents
+
+    2) PicToAscii.py: A python program which takes a jpg image and turns it into ascii art. 
+
+Description:
+
+    jpg_to_ascii is essentially what is stated in the title: a program that converts jpg images into ascii text. I wrote this program in python because it was all I knew how to use at the time and it was just a simple project. Someday I will probably update this to work for other image types, but for now it just works for jpgs. Another improvement to make would be for the program to automatically open the new ascii file.
+
+How To:
+
+    After downloading the python script (jgp_to_ascii.py) run:
+
+    $ python3 PicToAscii.py <jpgfilename>.jpg
+
+    in a directory that contains both the python script as well as the jpg
+
+    The script will have created a new file in this directory named <jpgfilename>Ascii.txt
